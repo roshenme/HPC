2cf8571a00e62a8ccd7f91edf7a572a8cba1a6d8
roshenme <roshenme@hotmail.com>
2016-03-28 22:06:56 +0100

    Task_3_Part_d
    The C++ Main program has been modified in order to run the python script
    for different arguments for the Crank-Nicolson scheme to plot the
    solution convergence with dx and dt

--- Task_3/Main.cpp
+++ Task_3/Main.cpp
@@ -9,84 +9,27 @@
 #include<iostream>
 #include<vector>
 #include "TriMatrix.h"
+#include<fstream>
+#include<math.h>
 
 using namespace std;
 
 // Start of the Main program
 
-int main()
+int main(int args, char** argv)
 {
 	float L, Nx, T, Nt, a, theta;		// Declare L, Nx, T, Nt, a as floating point numbers 
 
-	// Enter and Input L = The length of the domain
+	// Variables are to be provided as arguments in the python program
 
-	do
-	{
-		cout << "Enter the length of the domain  ";	// Ask for L in the command window
-		cin >> L;									// Assign the value to L
-		if (L == 0 || cin.fail())					// Check whether the input is a number and non-zero
-		{											// Otherwise display the following error message	
-			cout << "Error: The length of the domain must be a number and non-zero." << endl << endl;
-			cin.clear();
-			cin.ignore();
-		}
-	} while (L == 0 || cin.fail());
+	L = atof(argv[1]);
+	Nx = atof(argv[2]);
+	T = atof(argv[3]);
+	Nt = atof(argv[4]);
+	a = atof(argv[5]);
+	//theta= atof(argv[6]);
 
-	// Enter and Input Nx = The number of nodes
-
-	do
-	{
-		cout << "Enter the number of nodes  ";		// Ask for Nx in the command window
-		cin >> Nx;									// Assign the value to Nx
-		if (Nx == 0 || cin.fail())					// Check whether the input is a number and non-zero
-		{											// Otherwise display the following error message	
-			cout << "Error: The number of nodes must be a number and non-zero." << endl << endl;
-			cin.clear();
-			cin.ignore();
-		}
-	} while (Nx == 0 || cin.fail());
-
-	// Enter and Input T = The final time
-
-	do
-	{
-		cout << "Enter the final time  ";			// Ask for T in the command window
-		cin >> T;									// Assign the value to T
-		if (T == 0 || cin.fail())					// Check whether the input is a number and non-zero
-		{											// Otherwise display the following error message	
-			cout << "Error: The final time must be a number and non-zero." << endl << endl;
-			cin.clear();
-			cin.ignore();
-		}
-	} while (T == 0 || cin.fail());
-
-	// Enter and Input Nt = The number of time steps
-
-	do
-	{
-		cout << "Enter the number of time steps  ";	// Ask for Nt in the command window
-		cin >> Nt;									// Assign the value to Nt
-		if (Nt == 0 || cin.fail())					// Check whether the input is a number and non-zero
-		{											// Otherwise display the following error message	
-			cout << "Error: The number of time steps must be a number and non-zero." << endl << endl;
-			cin.clear();
-			cin.ignore();
-		}
-	} while (Nt == 0 || cin.fail());
-
-	// Enter and Input a = Thermal coefficient
-
-	do
-	{
-		cout << "Enter the thermal coefficient  ";	// Ask for a in the command window
-		cin >> a;									// Assign the value to Nt
-		if (a == 0 || cin.fail())					// Check whether the input is a number and non-zero
-		{											// Otherwise display the following error message	
-			cout << "Error: The thermal coefficient must be a number and non-zero." << endl << endl;
-			cin.clear();
-			cin.ignore();
-		}
-	} while (a == 0 || cin.fail());
+	theta = 0.5;
 
 	// Calculate dx, dt and v
 
@@ -118,8 +61,8 @@ int main()
 	U_O[Nx] = 0;							// Boundary condition when x=L
 	for (int i = 1; i < Nx; i++)			// Start the loop to go through all the nodes
 	{
-		U_O[i] = x[i] * (1 - x[i]);				// Calculate and assign the temperature at each node using the given function
-		//U_O[i] = sin((22 / 7)*x[i] / L);	// Calculate and assign the temperature at each node using the given function
+		//U_O[i] = x[i] * (1 - x[i]);				// Calculate and assign the temperature at each node using the given function
+		U_O[i] = sin((22 / 7)*x[i] / L);	// Calculate and assign the temperature at each node using the given function
 		cout << U_O[i] << " ";				// Output intial conditions vector
 	}
 	cout << endl << endl;
@@ -127,17 +70,17 @@ int main()
 	// Prompt user to choose which scheme the user wants use to solve the system
 	// The Forward Euler, Backward Euler or the Crank-Nicolson Schemes
 
-	char cont = 'y';
+	//char cont = 'y';
 
-	while (cont == 'y')
-	{
-		cout << "Enter value for theta" << endl << "0 for the Forward Euler Scheme" << endl << "1 for the Backward Euler Scheme" << endl << " 0.5 for the Crank-Nicolson Scheme";
-		cin >> theta;
-		while (theta != 0.0 && theta != 0.5 && theta != 1.0)
-		{
-			cout << "Theta has to be 0,1 or 0.5. Please enter the value again!";
-			cin >> theta;
-		}
+	//while (cont == 'y')
+	//{
+	//	cout << "Enter value for theta:" << endl << endl << "0 for the Forward Euler Scheme " << endl << "1 for the Backward Euler Scheme " << endl << "0.5 for the Crank-Nicolson Scheme ";
+	//	cin >> theta;
+	//	while (theta != 0.0 && theta != 0.5 && theta != 1.0)
+	//	{
+	//		cout << "Theta has to be 0,1 or 0.5. Please enter the value again!";
+	//		cin >> theta;
+	//	}
 
 		// Initialise the diagonals of A
 
@@ -280,34 +223,34 @@ int main()
 			cout << UL[i] << " ";		// Output the temperature at L/2 at all time steps in vector form
 		}
 		
-		cout << endl << endl;
-		cout << endl << "Would you like to continue? (y=Yes,n=No)";
-		cin >> cont;
-		cout << endl;
-	}
+		// Write the time vector and UL vector into a .txt file to be used by python
 
-	//// Write the time vector and UL vector into a .txt file to be used by python
+		ofstream file;
+		file.open("Temperaturehalf.txt");	// Open the file
 
-	//ofstream file;
-	//file.open("Temperaturehalf.txt");	// Open the file
+		for (int i = 0; i < Nt + 1; i++)
+		{
+			file << i*dt << "\t" << UL[i] << endl;	// Write the time and temperature of L/2 at each time step
+		}
 
-	//for (int i = 0; i < Nt+1; i++)
-	//{
-	//	file << i*dt << "\t" << UL[i] << endl;	// Write the time and temperature of L/2 at each time step
+		file.close();	// Close the file
+
+						// Write the U vector into a .txt file to be used by python
+
+		file.open("Temperature.txt");		// Open the file
+
+		for (int i = 0; i < Nx + 1; i++)
+		{
+			file << U[i] << endl;			// Write one value at a time in a loop
+		}
+
+		file.close();	// Close the file
+
+	//	cout << endl << endl;
+	//	cout << endl << "Would you like to continue? (y=Yes,n=No)";
+	//	cin >> cont;
+	//	cout << endl;
 	//}
-
-	//file.close();	// Close the file
-
-	//// Write the U vector into a .txt file to be used by python
-
-	//file.open("Temperature.txt");		// Open the file
-
-	//for (int i = 0; i < Nx+1; i++)
-	//{
-	//	file << U[i] << endl;			// Write one value at a time in a loop
-	//}
-
-	//file.close();	// Close the file
 
 	system("pause");
 
---
+++ Task_3/Task_3.py
@@ -0,0 +1,224 @@
+############################################
+##### Analytical vs Numerical Solution #####
+############################################
+
+# Import the libraries to run the C++ program and to plot the graphs
+
+import subprocess 
+import matplotlib.pyplot as mp
+import numpy as np
+
+# Provide variables as the arguments to the script in string format
+
+L=1           # Length of the domain
+Nx=20         # Number of nodes  
+T=1           # Final time
+Nt=1000       # Number of time steps
+a=1           # Thermal Coefficient
+   
+
+# Convert the variables to strings
+
+L=str(L)
+Nx=str(Nx)
+T=str(T)
+Nt=str(Nt)
+a=str(a)
+
+# Use the subspace module
+subprocess.call(["Main.exe",L,Nx,T,Nt,a],shell=True)
+
+file=open("Temperaturehalf.txt","r")                # Open the .txt file with all the temperatures
+t1,Numerical_Sol1 = np.loadtxt(file,unpack=True)    # Read the values and assign to Temp
+file.close()                                        # Close the file
+
+# Convert the strings into floating point numbers
+
+L=float(L)
+Nx=float(Nx)
+T=float(T)
+Nt=float(Nt)
+a=float(a)
+x_mid=L/2       # Define the mid point of the domain
+
+# Start the loop to calculate the Analytical solution using the given sin function
+
+t=np.arange(0,T+T/Nt,T/Nt)
+An_Sol=0
+Analytical_Sol1=[]
+
+for i in t:
+    An_Sol=np.sin(np.pi*x_mid/L)*np.exp(-i*a*(np.pi/L)**2)
+    Analytical_Sol1.append(An_Sol)
+
+# Form the final Analytical solution in the loop
+
+mp.plot(t1, Analytical_Sol1,label="Analytical Solution")                    # Plot the Analytical Solution
+mp.title('Temperature decay for the Analytical and Numerical Solutions')    # Title of plot
+mp.xlabel('Time')                                                           # Label x-axis
+mp.ylabel('Temperature')                                                    # Label y-axis
+mp.plot(t, Numerical_Sol1,label="Numerical Solution")                       # Plot the Numerical Solution
+mp.legend(loc=1)                                                            # Add legend to the graph
+mp.show()                                                                   # Show the graph
+
+# Compute the error between the analytical and numerical solutions
+
+Err1=Analytical_Sol1-Numerical_Sol1
+
+# The Analytical and Numerical Solutions can be compared
+
+mp.plot(t, Err1)                                                # Plot the Analytical Solution
+mp.title('Error between Analytical and Numerical Solutions')    # Title of plot
+mp.xlabel('Time')                                               # Label x-axis
+mp.ylabel('Temperature Error')                                  # Label y-axis
+mp.legend(loc=2)                                                # Add legend to the graph
+mp.show()                                                       # Show the graph
+
+#############################################################################
+#############################################################################
+#############################################################################
+
+# Run the program at different Nx values
+
+# Provide the Nx values as strings
+
+Nxx=["4","5","8","10","16","20"]
+
+# Start the loop for the different values of Nx
+RMSx=[]             # Initialise empty array to store RMS value
+dxx=[]              # Initialise empty array to store different dx value corresponding to the different Nx value
+for i in Nxx:       # Start the loop to go through the provided values of Nx
+
+    # Convert the arguments to string 
+
+    L=str(L)  
+    T=str(T)   
+    Nt=str(Nt)  
+    a=str(a)    
+
+    subprocess.call(["Main.exe",L,i,T,Nt,a])         # Use the subspace module
+
+    file=open("Temperature.txt","r")                 # Open the .txt file with all the temperatures
+    Numerical_Sol2= np.loadtxt(file,unpack=True)     # Read the values and assign to Temp vector
+    file.close()                                     # Close the file
+
+    # Convert the strings into floating point numbers
+
+    L=float(L)
+    Nxnew=float(i)
+    T=float(T)
+    Nt=float(Nt)
+    a=float(a)
+
+    # Generate the values of x for the current Nx in the loop
+
+    X=np.arange(0,L+L/Nxnew,L/Nxnew)
+
+    # Do a loop to calculate the analytical solution at all the nodes for the current Nx
+
+    U_Analytical=0              # Initialise the analytical solution as zero
+    Analytical_Sol2=[]           # Initialise an empty array to store the analytical solutions
+    for j in X:
+        U_Analytical=np.sin(np.pi*(j/L))*np.exp(-T*a*(np.pi/L)**2) 
+        Analytical_Sol2.append(U_Analytical)
+
+    # Calculate the error between the Analytical and Numerical solutions
+
+    Err2=Numerical_Sol2-Analytical_Sol2
+
+    # Create the vector with the different dx values
+
+    dx=L/Nxnew
+    dxx.append(dx)
+
+    # Calculate the RMS error for the current vector
+    
+    RMS=np.sqrt(np.mean(np.square(Err2)))
+
+    # Assemble the value to RMSx which will store the RMS error for different Nx values
+
+    RMSx.append(RMS)
+
+# Plot RMSx against the dx values
+
+mp.plot(dxx, RMSx,'ro')                                                     # Plot the Analytical Solution
+mp.title('RMS Error of the solution for different mesh sizes dx')           # Title of plot
+mp.xlabel('dx')                                                             # Label x-axis
+mp.ylabel('RMS Error')                                                      # Label y-axis
+mp.legend(loc=2)                                                            # Add legend to the graph
+mp.show()
+
+#############################################################################
+#############################################################################
+#############################################################################
+
+# Run the program at different Nt values
+
+# Provide the Nt values as strings
+
+Ntt=["800","900","1000","1100"] #"1200","1400","1500"]
+    
+# Start the loop for the different values of Nt
+RMSt=[]             # Initialise empty array to store RMS value
+dtt=[]              # Initialise empty array to store different dx value corresponding to the different Nx value
+for i in Ntt:       # Start the loop to go through the provided values of Nx
+
+    # Convert the arguments to string 
+
+    L=str(L)  
+    Nx=str(Nx)  
+    T=str(T)   
+    a=str(a)    
+
+    subprocess.call(["Main.exe",L,Nx,T,i,a])         # Use the subspace module
+
+    file=open("Temperaturehalf.txt","r")             # Open the .txt file with all the temperatures
+    t3,Numerical_Sol3= np.loadtxt(file,unpack=True)  # Read the values and assign to Temp vector
+    file.close()                                     # Close the file
+
+    # Convert the strings into floating point numbers
+
+    L=float(L)
+    Nx=float(Nx)
+    T=float(T)
+    Ntnew=float(i)
+    a=float(a)
+    x_mid=L/2       # Define the mid point of the domain
+
+    # Generate the values of x for the current Nx in the loop
+
+    tnew=np.arange(0,T+T/Ntnew,T/Ntnew)
+    
+    # Do a loop to calculate the analytical solution at all the nodes for the current Nx
+
+    U_Analytical=0              # Initialise the analytical solution as zero
+    Analytical_Sol3=[]           # Initialise an empty array to store the analytical solutions
+    for j in tnew:
+        U_Analytical=np.sin(np.pi*(x_mid/L))*np.exp(-j*a*(np.pi/L)**2) 
+        Analytical_Sol3.append(U_Analytical)
+
+    # Calculate the error between the Analytical and Numerical solutions
+
+    Err3=Numerical_Sol3-Analytical_Sol3
+
+    # Create the vector with the different dx values
+
+    dt=T/Ntnew
+    dtt.append(dt)
+
+    # Calculate the RMS error for the current vector
+    
+    RMSnew=np.sqrt(np.mean(np.square(Err3)))
+
+    # Assemble the value to RMSx which will store the RMS error for different Nx values
+
+    RMSt.append(RMSnew)
+
+# Plot RMSx against the dx values
+
+mp.plot(dtt, RMSt,'ro')                                                     # Plot the Analytical Solution
+mp.title('RMS Error of the solution for different time step sizes')         # Title of plot
+mp.xlabel('dt')                                                             # Label x-axis
+mp.ylabel('RMS Error')                                                      # Label y-axis
+mp.legend(loc=2)                                                            # Add legend to the graph
+mp.show()
