d59ec4f26d6be6a9e406c36321551fbd9ab58c7b
roshenme <roshenme@hotmail.com>
2016-03-28 12:11:58 +0100

    Task_2_Part_b
    Different Analytical Solution used and the comparison between the
    analytical and numerical solutions

--- Task_2/Task_2.py
+++ Task_2/Task_2.py
@@ -1,4 +1,14 @@
-#Provide the arguments as strings
+############################################
+##### Analytical vs Numerical Solution #####
+############################################
+
+# Import the libraries to run the C++ program and to plot the graphs
+
+import subprocess 
+import matplotlib.pyplot as mp
+import numpy as np
+
+# Provide variables as the arguments to the script in string format
 
 L="1"           # Length of the domain
 Nx="20"         # Number of nodes  
@@ -6,23 +16,12 @@ T="1"           # Final time
 Nt="1000"       # Number of time steps
 a="1"           # Thermal Coefficient
 
-#Import the libraries to plot
-
-import subprocess 
-import matplotlib.pyplot as mp
-import numpy as np
-
-# Plot the L/2 solution over time
-
 # Use the subspace module
-lines=subprocess.call(["Main.exe",L,Nx,T,Nt,a])
+subprocess.call(["Main.exe",L,Nx,T,Nt,a],shell=True)
 
-file=open("Temperature.txt","r")    # Open the .txt file with all the temperatures
-Temp = file.read().split('\n')      # Read the values and assign to Temp
-file.close()                        # Close the file
-Temp.pop()                          # Delete the last value
-
-Numerical_Sol=[float (i.strip()) for i in Temp]
+file=open("Temperaturehalf.txt","r")                # Open the .txt file with all the temperatures
+ti,Numerical_Sol = np.loadtxt(file,unpack=True)          # Read the values and assign to Temp
+file.close()                                        # Close the file
 
 # Convert the strings into floating point numbers
 
@@ -31,29 +30,41 @@ Nx=float(Nx)
 T=float(T)
 Nt=float(Nt)
 a=float(a)
-x_mid=0.5
+x_mid=L/2       # Define the mid point of the domain
 
-#Start the loop to calculate the Analytical solution using the given sin function
+# Start the loop to calculate the Analytical solution using the given sin function
 
 t=np.arange(0,T+T/Nt,T/Nt)
+An_Sol=0
+Analytical_Sol=[]
 
-def u(x,t):
-    if t==0:
-        return float(x*(1-x))
-    else:
-        val=0.0
-        for i in range(1,100,2):
-            val+=8.0/(np.pi**3)/(i**3)*np.sin(i*np.pi*x)*(np.e**(-(i**2)*(np.pi**2)*t)) 
-            return val
+for i in t:
+    An_Sol=np.sin(np.pi*x_mid/L)*np.exp(-i*a*(np.pi/L)**2)
+    Analytical_Sol.append(An_Sol)
 
-# Form the final Anayltical solution in the loop
+# Form the final Analytical solution in the loop
 
-Analytical_sol=[u(x_mid,i) for i in t]
-
-mp.plot(t, Analytical_sol,label="Analytical Solution")                      # Plot the Analytical Solution
+mp.plot(ti, Analytical_Sol,label="Analytical Solution")                     # Plot the Analytical Solution
 mp.title('Temperature decay for the Analytical and Numerical Solutions')    # Title of plot
 mp.xlabel('Time')                                                           # Label x-axis
 mp.ylabel('Temperature')                                                    # Label y-axis
 mp.plot(t, Numerical_Sol,label="Numerical Solution")                        # Plot the Numerical Solution
 mp.legend(loc=1)                                                            # Add legend to the graph
 mp.show()                                                                   # Show the graph
+
+# Compute the error between the analytical and numerical solutions
+
+Err=Analytical_Sol-Numerical_Sol
+
+# The Analytical and Numerical Solutions can be compared
+
+mp.plot(t, Err)                                                 # Plot the Analytical Solution
+mp.title('Error between Analytical and Numerical Solutions')    # Title of plot
+mp.xlabel('Time')                                               # Label x-axis
+mp.ylabel('Temperature Error')                                  # Label y-axis
+mp.legend(loc=2)                                                # Add legend to the graph
+mp.show()                                                       # Show the graph
+
+#############################################################################
+#############################################################################
+#############################################################################
