e968b2f75fc7a5951ad001ac8236e23d7a1765e4
roshenme <roshenme@hotmail.com>
2016-03-28 10:18:57 +0100

    Task_1_Part_a_Update
    TriMatrix Class populating from the main program

--- Task_1/Main.cpp
+++ Task_1/Main.cpp
@@ -9,17 +9,14 @@
 #include<iostream>
 #include<vector>
 #include "TriMatrix.h"
-#include "Main.h"
 
 using namespace std;
 
 // Start of the Main program
 
-int main()						
-{					
-
-	float L, T, a;				// Declare L, T and a as floating point numbers 
-	int Nx, Nt;					// Declare Nx and Nt as integers 
+int main()
+{
+	float L, Nx, T, Nt, a;		// Declare L, Nx, T, Nt, a as floating point numbers 
 
 	// Enter and Input L = The length of the domain
 
@@ -77,7 +74,7 @@ int main()
 		}
 	} while (Nt == 0 || cin.fail());
 
-		// Enter and Input a = Thermal coefficient
+	// Enter and Input a = Thermal coefficient
 
 	do
 	{
@@ -93,41 +90,64 @@ int main()
 
 	// Calculate dx, dt and v
 
-	float dx, dt, v;	// Declare the variables
+	float dx, dt, v;		// Declare the variables
 
 	dx = L / Nx;			// Mesh size is length of domain divided by the number of nodes
 	dt = T / Nt;			// Time step size if final time divided by the number of time steps
 	v = (a*dt) / (dx*dx);	// Constant
 
-	// Define the x vector with the position co-ordinates of the nodes
+							// Define the x vector with the position co-ordinates of the nodes
 
 	cout << endl;
 	cout << "The position co-ordinates vector" << endl << endl;		// Print on output screen
 
-	vector<float> x(Nx + 1);		// Initialise the x vector
+	vector<float> x(Nx + 1);			// Initialise the x vector
 	for (int i = 0; i <= Nx; i++)		// Start the loop to go through all the nodes
 	{
 		x[i] = i*dx;					// Calculate and assign the x co-ordinates
 		cout << x[i] << " ";			// Output the x co-ordinates vector
 	}
-	cout << endl << endl;				
+	cout << endl << endl;
 
 	// Define the initial conditions vector
 
 	cout << "The initial temperature vector" << endl << endl;	// Print on output screen
 
-	vector<float> U_O(Nx + 1);		// Initialise the intial conditions vector
+	vector<float> U_O(Nx + 1);			// Initialise the intial conditions vector
 	for (int i = 0; i <= Nx; i++)		// Start the loop to go through all the nodes
 	{
-		U_O[i] = x[i]*(1-x[i]);			// Calculate and assign the temperature at each node using the given function
+		U_O[i] = x[i] * (1 - x[i]);		// Calculate and assign the temperature at each node using the given function
 		cout << U_O[i] << " ";			// Output intial conditions vector
 	}
 	cout << endl << endl;
 
 	// Set the coefficient matrix A using the TriMatrix Class
 
-	TriMatrix Tri;			// Instantiate the class
-	Tri.set_A(v, Nx);		// Populate the the Trimatrix class
+	vector<double> MainDiag(Nx + 1);	// Initialize the main diagonal
+	vector<double> SubDiag(Nx);			// Initialize the sub diagonal
+	vector<double> SuperDiag(Nx);		// Initialize the super diagonal
+
+	MainDiag[0] = 1;					// The first value of the main diagonal is 1
+	MainDiag[Nx] = 1;					// The last value of the main diagonal is 1
+	MainDiag[Nx - 1] = 1 - 2 * v;		// The second last value of the main diagonal is 1-2v
+
+	SubDiag[0] = v;						// The first value of the sub diagonal is v
+	SubDiag[Nx - 1] = 0;				// The last value of the sub diagonal is 0
+
+	SuperDiag[0] = 0;					// The first value of the sub diagonal is 0
+	SuperDiag[Nx - 1] = v;				// The last value of the sub diagonal is v
+
+	for (int i = 1; i < Nx - 1; i++)
+	{
+		MainDiag[i] = 1 - 2 * v;		// Fill in the rest of the values of the main diagonal with 1-2v
+		SubDiag[i] = v;					// Fill in the rest of the values of the sub diagonal with v
+		SuperDiag[i] = v;				// Fill in the rest of the values of the superdiagonal with v
+	}
+	
+	// Instantiate and populate the TriMatrix class
+
+	TriMatrix Tri;									// Instantiate the class
+	Tri.set_A(SuperDiag, MainDiag, SubDiag);		// Populate the the TriMatrix class
 
 	// Initialise the Un and U vectors that will form the time iteration loop
 
@@ -148,7 +168,7 @@ int main()
 	// Display the final temperature vector
 
 	cout << "The final temperature vector" << endl << endl;		// Print on output screen
-	
+
 	for (int i = 0; i<Nx + 1; i++)
 	{
 		cout << U[i] << " ";		// Output the final temperature vector
@@ -156,7 +176,6 @@ int main()
 	cout << endl << endl;
 
 	system("pause");
-	
+
 	return 0;
 }
-
